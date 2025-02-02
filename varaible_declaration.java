0f818fd27e9eb5cca96cd5073e23fcd45abd8d01
StevenDesigner <stephenrajprime@gmail.com>
2025-02-02 14:21:12 +0530

    variable declarataion

---
+++ varaible_declaration.java
@@ -0,0 +1,10 @@
+public class varaible_declaration {
+    public static void main(String[] args) {
+        int x;  // Variable declaration
+        x = 10; // Assigning value
+        System.out.println(x); // Output: 10
+
+        double y = 5.99; // Declaring and initializing
+        System.out.println(y); // Output: 5.99
+    }
+}
