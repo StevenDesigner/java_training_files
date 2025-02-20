aa14496ff40eea9e9e874e2d314567e9ee6cc43e
StevenDesigner <stephenrajprime@gmail.com>
2025-02-20 21:54:30 +0530

    number check and month switch

--- varaible_declaration.java
+++ varaible_declaration.java
@@ -1,20 +1,20 @@
-public class varaible_declaration {
-    public static void main(String[] args) {
-        int x;  // Variable declaration
-        x = 10; // Assigning value
-        System.out.println(x); // Output: 10
+// public class varaible_declaration {
+//     public static void main(String[] args) {
+//         int x;  // Variable declaration
+//         x = 10; // Assigning value
+//         System.out.println(x); // Output: 10
 
-        double y = 5.99; // Declaring and initializing
-        System.out.println(y); // Output: 5.99
-    }
-}
+//         double y = 5.99; // Declaring and initializing
+//         System.out.println(y); // Output: 5.99
+//     }
+// }
 
 
 // Declare and initialize variables for name, age, marks, and grade. Print them.
 
 // public class varaible_declaration {
 //     public static void main(String[] args) {
-//         String name = "John"; // String variable
+//         String name = "John";  //String variable    
 //         int age = 20;         // Integer variable
 //         double marks = 85.5;  // Double variable
 //         char grade = 'A';     // Char variable
@@ -29,15 +29,15 @@ public class varaible_declaration {
 
 // Convert an int to a double using implicit casting.
 
-// public class varaible_declaration {
-//     public static void main(String[] args) {
-//         int num = 50;        // Integer value
-//         double converted = num;  // Implicit conversion to double
+public class varaible_declaration {
+    public static void main(String[] args) {
+        int num = 50;        // Integer value
+        double converted = num;  // Implicit conversion to double
         
-//         System.out.println("Integer Value: " + num);
-//         System.out.println("Converted to Double: " + converted);
-//     }
-// }
+        System.out.println("Integer Value: " + num);
+        System.out.println("Converted to Double: " + converted);
+    }
+}
 
 
 // public class varaible_declaration {
