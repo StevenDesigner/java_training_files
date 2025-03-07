30b0ed5a256ac2356d057341c8450d2a5094f703
stevenDesigner <stephenrajprime@gmail.com>
2025-03-07 19:42:29 +0530

    switch example

---
+++ Switchsample.java
@@ -0,0 +1,21 @@
+public class Switchsample {
+    public static void main(String[] args) {
+
+        String day="ramu";
+        
+        switch(day){
+            case "ramu":
+            System.out.println("sunday");
+            break;
+            case "raju":
+            System.out.println("monday");
+            break;
+            case "steven":
+            System.out.println("tuesday");
+            break;
+            default:
+            System.out.println("invalid data");
+        }
+
+    }
+}
