d5fc3952b25d2c54312703e45b51998c115d701a
StevenDesigner <stephenrajprime@gmail.com>
2025-06-18 20:47:59 +0530

    sorting

---
+++ sorting/BubbleSortExample.java
@@ -0,0 +1,36 @@
+package sorting;
+
+public class BubbleSortExample {
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
---
+++ strings/GroupAnagrams.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+       if(strs==null || strs.length ==0 ) return  new ArrayList<>();
+       Map<String, List<String>> map = new HashMap<>();
+
+       for(String s: strs){
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        String sorted= new String(chars);
+
+        map.computeIfAbsent(sorted, k->new ArrayList()).add(s);
+       }
+
+
+       return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> grouped = groupAnagrams(input);
+
+       
+        for (List<String> group : grouped) {
+            System.out.println(group);
+        }
+    }
+}
